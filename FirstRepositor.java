651bb37f6ad34cde5d50c18210a3439073a43fa1
SvetlanaLu4 <SvetlanaLu4@yandex.ru>
2014-12-14 15:03:01 +0200

    changes in lab1

--- FirstRepositor.java
+++ FirstRepositor.java
@@ -1,7 +1,7 @@
 public class FirstRepositor {
-	public static void main (String [] args){
+	public static void main (String[] args){
 
-        System.out.println ("Hello world!)");
+   		System.out.println ("Hello world!)");
 	}
 }
 
