7271fa198f73bd52209520426c3f43eece00f962
SvetlanaLu4 <SvetlanaLu4@yandex.ru>
2014-12-15 07:58:40 +0200

    Dz2 gotovo=)

---
+++ Dz2Radius.java
@@ -0,0 +1,12 @@
+public class Dz2Radius{
+	public static void main (String[] args) {
+	int radius = 4;
+	int diameter = radius*2;
+	float dlinaOkruznosti = 3.14F*diameter;
+	float square = 3.14F*(radius*radius);
+		System.out.println ("Radius: "+ radius);
+		System.out.println ("Diameter: "+ diameter);
+		System.out.println ("Dlina Okruznosti: "+ dlinaOkruznosti);
+		System.out.println ("Square: "+ square);
+		}
+	}
