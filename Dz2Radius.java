0dd46abc1cde2364a49a75ff18aa3989766ef3c9
SvetlanaLu4 <SvetlanaLu4@yandex.ru>
2014-12-16 08:03:13 +0200

    all your comments I have corrected

--- Dz2Radius.java
+++ Dz2Radius.java
@@ -2,11 +2,11 @@ public class Dz2Radius{
 	public static void main (String[] args) {
 	int radius = 4;
 	int diameter = radius*2;
-	float dlinaOkruznosti = 3.14F*diameter;
+	float circleLenth = 3.14F*diameter;
 	float square = 3.14F*(radius*radius);
-		System.out.println ("Radius: "+ radius);
-		System.out.println ("Diameter: "+ diameter);
-		System.out.println ("Dlina Okruznosti: "+ dlinaOkruznosti);
-		System.out.println ("Square: "+ square);
-		}
+		System.out.println("Radius: "+ radius);
+		System.out.println("Diameter: "+ diameter);
+		System.out.println("Dlina Okruznosti: "+ circleLenth);
+		System.out.println("Square: "+ square);
 	}
+}
